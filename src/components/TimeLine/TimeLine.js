c14e61575a0c8a787f6b47cb661bd15294d1a5e4
Sam <noci0001@hz.nl>
2022-06-13 22:14:55 +0200

    update 3.3

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      I am a software engineer with proven experience in the creation of web applications and dynamic interactive websites. I am passionate about Front end engineering because I love art, being creative and at the same time being extremely technical.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
