03749de8d322bdcdea0ff93180ed492e10b741e2
Sam <noci0001@hz.nl>
2022-06-13 22:17:48 +0200

    remove projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -16,25 +16,7 @@ export const projects = [
     source: 'https://github.com/noci0001/Old_React_Personal_Site_On_Git',
     visit: 'https://sampersonalwebsite.netlify.app/',
     id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+  }
 ];
 
 export const TimeLineData = [
