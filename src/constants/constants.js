cd5a8d6c426bdec15aba8408216f7311ca3664d3
Sam <noci0001@hz.nl>
2023-10-02 13:42:52 +0200

    added projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,9 +3,9 @@ export const projects = [
     title: 'Minishell',
     description: "The Minishell 42 project was a significant and rewarding challenge in the 42 curriculum, pushing me to explore various aspects of software development. Despite my initial intimidation, it involved creating a command-line interpreter resembling Bash, focusing on core functionalities: Operating exclusively in interactive mode Executing commands with absolute or relative paths Supporting simple commands without paths Maintaining a command history Implementing pipes, redirections, and here-doc functionality Handling double and single quotes, environment variables, and signals Including built-in commands like echo, exit, env, export, unset, cd, and pwd. The project was divided into front-end and back-end sections. The front-end managed user input and commands, while signals were addressed later to reduce complexity. The parsing journey involved two phases: lexical analysis (tokenization) and syntax analysis (parsing). Lexical analysis transformed user input into tokens, emphasizing token types over values. Syntax analysis scanned tokens based on a predefined grammar, potentially generating an Abstract Syntax Tree. Lexing was relatively straightforward, while parsing posed a considerable challenge.",
     image: '/images/1.png',
-    tags: ['React', 'Node', 'Figma', 'Netlify'],
-    source: 'https://moderndesignwebsite.netlify.app/',
-    visit: 'https://github.com/noci0001/Modern_Design_Website',
+    tags: ['C', 'C++', 'Teamworking', 'Backend'],
+    source: 'https://github.com/noci0001/42_Minishell',
+    visit: 'https://github.com/noci0001/42_Minishell',
     id: 0,
   },
   {
